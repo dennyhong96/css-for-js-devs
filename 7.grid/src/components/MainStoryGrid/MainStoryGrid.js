75e76d99825ce3217be4d836024af553f00d60ea
Denny Hong <hong961127@gmail.com>
2021-08-25 03:19:33 -0400

    Implement MainStoryGrid

--- 7.grid/src/components/MainStoryGrid/MainStoryGrid.js
+++ 7.grid/src/components/MainStoryGrid/MainStoryGrid.js
@@ -8,6 +8,7 @@ import MainStory from "../MainStory";
 import SecondaryStory from "../SecondaryStory";
 import OpinionStory from "../OpinionStory";
 import Advertisement from "../Advertisement";
+import { QUERIES } from "../../constants";
 
 const MainStoryGrid = () => {
   return (
@@ -19,22 +20,22 @@ const MainStoryGrid = () => {
       <SecondaryStorySection>
         <StoryList>
           {SECONDARY_STORIES.map((story, index) => (
-            <VerticalStorayWrapper key={story.id}>
+            <VerticalStoryWrapper key={story.id}>
               <SecondaryStory {...story} />
-            </VerticalStorayWrapper>
+            </VerticalStoryWrapper>
           ))}
         </StoryList>
       </SecondaryStorySection>
 
       <OpinionSection>
         <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
+        <OpinionsStoryList>
           {OPINION_STORIES.map((story, index) => (
-            <VerticalStorayWrapper key={story.id}>
+            <OpinionsStoryWrapper key={story.id}>
               <OpinionStory {...story} />
-            </VerticalStorayWrapper>
+            </OpinionsStoryWrapper>
           ))}
-        </StoryList>
+        </OpinionsStoryList>
       </OpinionSection>
 
       <AdvertisementSection>
@@ -53,14 +54,43 @@ const Wrapper = styled.div`
     "advertisement";
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    gap: 48px 0;
+    grid-template-columns: 2fr 1fr;
+    grid-template-areas:
+      "main-story secondary-stories"
+      "advertisement advertisement"
+      "opinion-stories opinion-stories";
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    gap: 0;
+    grid-template-columns: 5fr 4fr 3fr;
+    grid-template-areas:
+      "main-story secondary-stories opinion-stories"
+      "main-story  advertisement advertisement";
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-right: 16px;
+    padding-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const StoryList = styled.div`
@@ -68,7 +98,7 @@ const StoryList = styled.div`
   flex-direction: column;
 `;
 
-const VerticalStorayWrapper = styled.div`
+const VerticalStoryWrapper = styled.div`
   &:not(:last-of-type) {
     border-bottom: 1px solid var(--color-gray-300);
     padding-bottom: 16px;
@@ -76,12 +106,40 @@ const VerticalStorayWrapper = styled.div`
   }
 `;
 
+const OpinionsStoryList = styled(StoryList)`
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: row;
+    gap: 32px;
+  }
+`;
+
+const OpinionsStoryWrapper = styled(VerticalStoryWrapper)`
+  @media ${QUERIES.tabletOnly} {
+    flex: 1;
+    &:not(:last-of-type) {
+      border-bottom: revert;
+      padding-bottom: revert;
+      margin-bottom: revert;
+    }
+  }
+`;
+
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    transform: translateY(-8px);
+  }
 `;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: 16px;
+    padding-top: 16px;
+    border-top: 1px solid var(--color-gray-300);
+  }
 `;
 
 export default MainStoryGrid;
--- 7.grid/src/components/SecondaryStory/SecondaryStory.js
+++ 7.grid/src/components/SecondaryStory/SecondaryStory.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
   return (
@@ -23,6 +24,14 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-columns: 100%;
+    grid-template-areas:
+      "image"
+      "heading"
+      "abstract";
+  }
 `;
 
 const Image = styled.img`
@@ -32,6 +41,10 @@ const Image = styled.img`
   height: 120px;
   border-radius: 4px;
   object-fit: cover;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-bottom: 8px;
+  }
 `;
 
 const Heading = styled.h2`
